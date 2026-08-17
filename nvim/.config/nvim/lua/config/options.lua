9828b514b50ca111269026072e5c9f9e43dd9869
Robin Quintero <rohaquinlop301@gmail.com>
2026-08-17 15:27:46 -0500

    nvim: zed parity tabs and soft wrap

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -9,3 +9,16 @@ vim.opt.cursorline = true -- cursorline = true
 vim.opt.colorcolumn = "80" -- rulers = [80]
 -- cursor-shape: underline in every mode + color-modes (colors via HelixCursor* groups)
 vim.opt.guicursor = "n:hor10-HelixCursorNormal,i:hor10-HelixCursorInsert,v:hor10-HelixCursorSelect,ve:hor10-HelixCursorSelect,o:hor10-HelixCursorNormal,r:hor10-HelixCursorInsert,c:hor10-HelixCursorNormal"
+
+-- Zed parity: tab_size = 4, soft_wrap = "editor_width" (LazyVim defaults: 2, off)
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.wrap = true
+-- Zed "TSX": tab_size = 2
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "typescriptreact" },
+  callback = function()
+    vim.bo.tabstop = 2
+    vim.bo.shiftwidth = 2
+  end,
+})
